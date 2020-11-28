1003dd41342b78a313a8bcf40f1edec12b13f392
Maciej Krywult <mkrywult88@gmail.com>
2020-11-28 17:46:38 +0100

    day 2 exercise 2

--- exercises/Day_2.py
+++ exercises/Day_2.py
@@ -1,4 +1,20 @@
+# #ex1:
+# lis = input('Podaj iczby: ').split(',')
+# print(lis)
+# print(tuple(lis))
+
 #ex1:
-lis = input('Podaj iczby: ').split(',')
-print(lis)
-print(tuple(lis))
+class InOutString:
+    def get_string(self):
+        self.stri = input("podaj wyraz: ")
+
+    def post_string(self):
+        print(self.stri.upper())
+
+    def lower_string(self):
+        print(self.stri.lower())
+
+xx = InOutString()
+xx.get_string()
+xx.post_string()
+xx.lower_string()
