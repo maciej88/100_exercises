dbaf1f0a4c785e0ec15e8bd5568f8d57ddbf6581
Maciej Krywult <mkrywult88@gmail.com>
2020-11-28 17:56:48 +0100

    day 2 exercise 3

--- exercises/Day_2.py
+++ exercises/Day_2.py
@@ -1,20 +1,33 @@
+from math import *
+
 # #ex1:
 # lis = input('Podaj iczby: ').split(',')
 # print(lis)
 # print(tuple(lis))
 
 #ex1:
-class InOutString:
-    def get_string(self):
-        self.stri = input("podaj wyraz: ")
+# class InOutString:
+#     def get_string(self):
+#         self.stri = input("podaj wyraz: ")
+#
+#     def post_string(self):
+#         print(self.stri.upper())
+#
+#     def lower_string(self):
+#         print(self.stri.lower())
+#
+# xx = InOutString()
+# xx.get_string()
+# xx.post_string()
+# xx.lower_string()
+#ex3
+#in this exercise I was empty of ideas.... but:
+C,H = 50,30
 
-    def post_string(self):
-        print(self.stri.upper())
+def calc(D):
+    D = int(D)
+    return str(int(sqrt((2*C*D)/H)))
 
-    def lower_string(self):
-        print(self.stri.lower())
-
-xx = InOutString()
-xx.get_string()
-xx.post_string()
-xx.lower_string()
+D = input().split(',')
+D = list(map(calc,D))   # applying calc function on D and storing as a list
+print(",".join(D))
