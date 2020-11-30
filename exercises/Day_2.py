5654055dabfeb943174aa46740cb7b614afe4dd3
Maciej Krywult <mkrywult88@gmail.com>
2020-11-30 17:38:09 +0100

    ex 8

--- exercises/Day_2.py
+++ exercises/Day_2.py
@@ -33,13 +33,18 @@ from math import *
 # print(",".join(d))
 
 #ex4 (7);
-x,y = map(int,input().split(','))
-ls = []
+# x,y = map(int,input().split(','))
+# ls = []
+#
+# for i in range(x):
+#     tmp = []
+#     for j in range(y):
+#         tmp.append(i*j)
+#     ls.append(tmp)
+#
+# print(ls)
 
-for i in range(x):
-    tmp = []
-    for j in range(y):
-        tmp.append(i*j)
-    ls.append(tmp)
-
-print(ls)
+#ex 5(8):
+lst = input().split(',')
+lst.sort()
+print(",".join(lst))
