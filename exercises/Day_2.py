e6ad87ad3b31dc94e49b30c4df3a3d2b878bd529
maciej <mkrywult88@gmail.com>
2022-08-18 21:09:19 +0200

    Q6 - math :/

--- exercises/Day_2.py
+++ exercises/Day_2.py
@@ -1,3 +1,4 @@
+from math import sqrt
 # Q4:
 def get_sequence(n):
     raw = n.split(',')
@@ -16,3 +17,17 @@ class StringConvert:
 x = StringConvert()
 x.get_string()
 x.print_string()
+
+#Q6:
+# Q = Sqr[(2*C*D)/H]
+C, H = 50, 30
+def calc(D):
+    return sqrt((2*C*D)/H)
+
+D = [int(i) for i in input("enter 3 numbers, egz: x,y,z: ").split(',')] # splits in comma position and set up in list
+D = [int(i) for i in D]   # converts string to integer
+D = [calc(i) for i in D]  # returns floating value by calc method for every item in D
+D = [round(i) for i in D] # All the floating values are rounded
+D = [str(i) for i in D]   # All the integers are converted to string to be able to apply join operation
+
+print(",".join(D))
