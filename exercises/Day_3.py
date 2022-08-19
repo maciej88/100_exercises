4d1ff64e3a723a5568271120383c51169926b94b
maciej <mkrywult88@gmail.com>
2022-08-19 19:54:42 +0200

    Q12 - math

--- exercises/Day_3.py
+++ exercises/Day_3.py
@@ -23,3 +23,11 @@ for i in data:
         lst.append(i)
 
 print(",".join(lst))
+
+#12:
+def check(element):
+    return all(ord(i)%2 == 0 for i in element)  # all returns True if all digits i is even in element
+
+lst = [str(i) for i in range(1000,3001)]        # creates list of all given numbers with string data type
+lst = list(filter(check,lst))                   # filter removes element from list if check condition fails
+print(",".join(lst))
