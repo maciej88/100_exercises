d66344a6cfc539f690c82cb4dfcabba672a08e9a
maciej <mkrywult88@gmail.com>
2022-08-19 19:53:09 +0200

    Q11 - math from binnary

--- exercises/Day_3.py
+++ exercises/Day_3.py
@@ -4,3 +4,22 @@ words = [word for word in x.split(' ')]
 words = sorted(words)
 words = list(dict.fromkeys(words))
 print(' '.join(words))
+
+#Q11:
+def check(x):                       # converts binary to integer & returns zero if divisible by 5
+    total,pw = 0,1
+    reversed(x)
+
+    for i in x:
+        total+=pw * (ord(i) - 48)   # ord() function returns ASCII value
+        pw*=2
+    return total % 5
+
+data = input().split(",")           # inputs taken here and splited in ',' position
+lst = []
+
+for i in data:
+    if check(i) == 0:               # if zero found it means divisible by zero and added to the list
+        lst.append(i)
+
+print(",".join(lst))
