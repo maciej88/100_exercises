d6b7e28fa36ece97019f3557cd36f60a6f002915
maciej <mkrywult88@gmail.com>
2022-08-20 20:07:53 +0200

    Q14

--- exercises/Day_4.py
+++ exercises/Day_4.py
@@ -5,13 +5,23 @@
 #         words.remove(i)
 #
 # words.sort()
-# print(' - '.join(words))
+# # print(' - '.join(words))
+#
+# #ex 11:
+# def test(n):
+#     return int(n,2)%5 == 0
+#
+# data = input("enter number sequence: ").split(',')
+#
+# data = list(filter(test,data))
+# print(",".join(data))
+#Q14:
+sentence = input('Give the sentence in upper and lowercase: ')
+upper = 0
+lower = 0
 
-#ex 11:
-def test(n):
-    return int(n,2)%5 == 0
+for i in sentence:
+    upper += i.isupper()
+    lower += i.islower()
 
-data = input("enter number sequence: ").split(',')
-
-data = list(filter(test,data))
-print(",".join(data))
+print(f"lower: {lower}, upper: {upper}")
