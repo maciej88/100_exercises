3e9db8ba35ffa012d66c912eef6f4b4912ef7ae0
maciej <mkrywult88@gmail.com>
2022-08-20 20:10:06 +0200

    Q15 - math :(

--- exercises/Day_4.py
+++ exercises/Day_4.py
@@ -25,3 +25,13 @@ for i in sentence:
     lower += i.islower()
 
 print(f"lower: {lower}, upper: {upper}")
+
+#Q15:
+a = input()
+total,tmp = 0,str()        # initialing an integer and empty string
+
+for i in range(4):
+    tmp+=a               # concatenating 'a' to 'tmp'
+    total+=int(tmp)      # converting string type to integer type
+
+print(total)
