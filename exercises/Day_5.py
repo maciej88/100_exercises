d4fb14a2b49b33d385987f28e135c9fb494ac3d1
maciej <mkrywult88@gmail.com>
2022-08-21 11:31:16 +0200

    Q17 - math to analise

--- exercises/Day_5.py
+++ exercises/Day_5.py
@@ -11,3 +11,18 @@ def get_odd():
     print(lst)
 
 get_odd()
+
+#Q17:
+total = 0
+while True:
+    s = input().split()
+    if not s:            # break if the string is empty
+        break
+    cm,num = map(str,s)    # two inputs are distributed in cm and num in string data type
+
+    if cm=='D':
+        total+=int(num)
+    if cm=='W':
+        total-=int(num)
+
+print(total)
