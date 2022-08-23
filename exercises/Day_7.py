e5d9c106f786d093ce8bb141d2e95ddda7a53add
maciej <mkrywult88@gmail.com>
2022-08-23 19:24:02 +0200

    Q21 math :(

--- exercises/Day_7.py
+++ exercises/Day_7.py
@@ -7,3 +7,23 @@ class DiviseGen():
 
 for i in DiviseGen().by_seven(int(input('Give number to divine: '))):
     print(i)
+
+#Q21:
+from math import sqrt
+lst = []
+position = [0,0]
+while True:
+    a = input()
+    if not a:
+        break
+    lst.append(a)
+for i in lst:
+    if 'UP' in i:
+        position[0] -= int(i.strip('UP '))
+    if 'DOWN' in i:
+        position[0] += int(i.strip('DOWN '))
+    if 'LEFT' in i:
+        position[1] -= int(i.strip('LEFT '))
+    if 'RIGHT' in i:
+        position[1] += int(i.strip('RIGHT '))
+print(round(sqrt(position[1] ** 2 + position[0] ** 2)))
