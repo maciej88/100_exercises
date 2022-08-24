97b7f126f4bfa7edfaae89d725d167c4df0dd473
maciej <mkrywult88@gmail.com>
2022-08-24 19:09:09 +0200

    Q23

--- exercises/Day_8.py
+++ exercises/Day_8.py
@@ -3,3 +3,10 @@ sentence = input('Sentence: ').split()
 words = sorted(set(sentence))
 for i in words:
     print(f"{i}: {sentence.count(i)}")
+
+#Q23:
+def calc():
+    num = int(input())
+    return print(num ** 2)
+
+calc()
