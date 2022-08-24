962fac7efe3f3f65d68bbb9e6d3229f173532ac9
maciej <mkrywult88@gmail.com>
2022-08-24 19:14:41 +0200

    Q24 ???

--- exercises/Day_8.py
+++ exercises/Day_8.py
@@ -10,3 +10,23 @@ def calc():
     return print(num ** 2)
 
 calc()
+
+#Q24:
+def help_me():
+    print(help.__doc__)
+    print(str.__doc__)
+    print(sorted.__doc__)
+
+help_me()
+
+def pow(n,p):
+    '''
+    param n: This is any integer number
+    param p: This is power over n
+    return:  n to the power p = n^p
+    '''
+
+    return n**p
+
+print(pow(3,4))
+print(pow.__doc__)
