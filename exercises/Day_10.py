5e27aa636354614810b1b23ba58dc3ef2cf0f050
maciej <mkrywult88@gmail.com>
2022-08-26 19:52:24 +0200

    Q32

--- exercises/Day_10.py
+++ exercises/Day_10.py
@@ -6,3 +6,13 @@ def get_dict():
     print(new_dict)
 
 get_dict()
+
+#Q32:
+def get_new_dict():
+    new_dict = {}
+    for i in range(1, 21):
+        new_dict[i] = i ** 2
+    for k in new_dict.keys():
+        print(k)
+
+get_new_dict()
