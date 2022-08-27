cc4f287a232422c1cd641afb957bba9d43d0e5e5
maciej <mkrywult88@gmail.com>
2022-08-27 19:27:04 +0200

    Q39

--- exercises/Day_11.py
+++ exercises/Day_11.py
@@ -7,3 +7,13 @@ def tpl_slice():
     print(n1, n2)
 
 tpl_slice()
+
+#Q39:
+tpl = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+li = []
+for i in tpl:
+    if i%2==0:
+        li.append(i)
+
+tpl2 = tuple(li)
+print(tpl2)
