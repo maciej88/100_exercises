c8d4416bbd069a0973af63078bdd71c41ce0537a
maciej <mkrywult88@gmail.com>
2022-08-27 19:43:48 +0200

    Q42

--- exercises/Day_11.py
+++ exercises/Day_11.py
@@ -35,3 +35,14 @@ def sqrs(item):
 
 lst = [i for i in range(1, 11)]
 print(list(map(sqrs, lst)))
+
+#Q42:
+def even(x):
+    return x%2==0
+
+def squer(x):
+    return x*x
+
+li = [1,2,3,4,5,6,7,8,9,10]
+li = map(squer,filter(even,li))   # first filters number by even number and the apply map() on the resultant elements
+print(list(li))
