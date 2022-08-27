74d8e9eddf2b5410e57d28a2e68f6d37affa6e85
maciej <mkrywult88@gmail.com>
2022-08-27 19:44:26 +0200

    Q43

--- exercises/Day_11.py
+++ exercises/Day_11.py
@@ -46,3 +46,10 @@ def squer(x):
 li = [1,2,3,4,5,6,7,8,9,10]
 li = map(squer,filter(even,li))   # first filters number by even number and the apply map() on the resultant elements
 print(list(li))
+
+#Q43:
+def even(x):
+    return x%2==0
+
+evenNumbers = filter(even, range(1,21))
+print(list(evenNumbers))
