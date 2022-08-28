6aac6e1cbe75a7aed9842ee06b0e0bc9011a2227
maciej <mkrywult88@gmail.com>
2022-08-28 15:39:27 +0200

    Q46

--- exercises/Day_12.py
+++ exercises/Day_12.py
@@ -4,14 +4,22 @@ def sqr_nums(items):
 
 print(list(map(sqr_nums, range(1, 21))))
 
-#Q45:
+#Q45 & Q46:
 class American(object):
     @staticmethod
     def printNationality():
         print('America')
 
+class NewYorker(American):
+    @staticmethod
+    def printCity():
+        print('New-York')
 
 if __name__ == "__main__":
     x = American()
     x.printNationality()
     American.printNationality()
+
+    y = NewYorker()
+    y.printNationality()
+    y.printCity()
