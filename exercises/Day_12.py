c4cf3600a18da5ae9458d4092f757e6eb1506fd9
maciej <mkrywult88@gmail.com>
2022-08-28 15:36:50 +0200

    Q45

--- exercises/Day_12.py
+++ exercises/Day_12.py
@@ -3,3 +3,15 @@ def sqr_nums(items):
     return items ** 2
 
 print(list(map(sqr_nums, range(1, 21))))
+
+#Q45:
+class American(object):
+    @staticmethod
+    def printNationality():
+        print('America')
+
+
+if __name__ == "__main__":
+    x = American()
+    x.printNationality()
+    American.printNationality()
