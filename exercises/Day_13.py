c25eeb3a2974ff0f0a69120a09d4bb93423329c5
maciej <mkrywult88@gmail.com>
2022-08-29 17:53:47 +0200

    Q48

--- exercises/Day_13.py
+++ exercises/Day_13.py
@@ -7,6 +7,24 @@ class Circle:
     def area(self):
         return 3.14*self.r**2
 
+#Q48:
+class Ractangle:
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
 if __name__ == "__main__":
     a = Circle(2)
     print(a.area())
+
+    b = Ractangle(2, 3)
+    print(b.area())
