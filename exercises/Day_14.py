0c4577fd709a09e4e047d4af093dcd08668aa5e4
maciej <mkrywult88@gmail.com>
2022-08-30 16:23:21 +0200

    Q52

--- exercises/Day_14.py
+++ exercises/Day_14.py
@@ -6,3 +6,22 @@ try:
     compute()
 except ZeroDivisionError:
     print("0? are You serius?")
+
+#Q52:
+class CustomError(Exception):
+    """New Error
+    Attributes:
+    err - Yes, You messed it up!
+    """
+    def __init__(self, err):
+        self.err = err
+
+exemple = int(input())
+
+try:
+    if exemple < 5:
+        raise CustomError('Ekhem')
+    elif exemple > 5:
+        raise CustomError("try 0")
+except CustomError as ce:
+    print("you made it " + ce.err)
