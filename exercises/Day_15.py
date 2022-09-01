3d7e3951fce9df3ae91db82879bd4fa39c40361f
maciej <mkrywult88@gmail.com>
2022-09-01 18:02:30 +0200

    Q59

--- exercises/Day_15.py
+++ exercises/Day_15.py
@@ -17,9 +17,20 @@ def unicode():
     print(sentence)
 
 #Q57:
-s = input("any string: ")
-u = s.encode('utf-8')
-print(u)
+# s = input("any string: ")
+# u = s.encode('utf-8')
+# print(u)
 
 #Q58:
 # -*- coding: utf-8 -*-
+#it should be on top of file.
+
+#Q59:
+def wierd():
+    n = int(input("number: "))
+    result = 0
+    for i in range(1, n+1):
+        result += float(i /float(i+1))
+        print(round(result, 2))
+
+wierd()
