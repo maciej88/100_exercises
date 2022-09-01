38a481ff4a2713fff7c8ace3224cdbf7a3bf3594
maciej <mkrywult88@gmail.com>
2022-09-01 17:50:09 +0200

    Q55

--- exercises/Day_15.py
+++ exercises/Day_15.py
@@ -1,7 +1,14 @@
+import re
+
 #Q54:
 def posted():
     email = input("enter email: ")
     end =email.split("@")[1]
     print(end.split(".")[0])
 
-posted()
+#Q55:
+def calculus():
+    n = input("sentence: ")
+    print(re.findall("\d+", n))
+
+calculus()
