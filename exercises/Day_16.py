02a490cc7f6571d980c79c3122ea31c09d18c2ca
maciej <mkrywult88@gmail.com>
2022-09-02 20:41:37 +0200

    Q63

--- exercises/Day_16.py
+++ exercises/Day_16.py
@@ -30,3 +30,12 @@ f(n)              # call once and it will set value to fibo[0-n]
 fibo = [str(i) for i in fibo]   # converting integer data to string type
 ans = ",".join(fibo)    # joining all string element of fibo with ',' character
 print(ans)
+
+#63:
+n = int(input("Give number: "))
+
+for i in range(0, n+1, 2):
+    if i < n - 1:
+        print(i, end=',')
+    else:
+        print(i)
