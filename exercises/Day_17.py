bfc0cb3ee8a3db1adb919a74e5893619d473226d
maciej <mkrywult88@gmail.com>
2022-09-03 17:34:16 +0200

    Q67

--- exercises/Day_17.py
+++ exercises/Day_17.py
@@ -6,3 +6,23 @@ for i in li:
 #Q66:
 val = input("Give to calculate: ")
 print(eval(val))
+
+#67:
+def binary_search(lst, item):
+    low = 0
+    high = len(lst) - 1
+
+    while low <= high:
+        mid = round((low + high) / 2)
+
+        if lst[mid] == item:
+            return mid
+        elif lst[mid] > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+lst = [1, 3, 5, 7, ]
+print(binary_search(lst, 9))
