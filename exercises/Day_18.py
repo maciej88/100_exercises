1e99985b8aa2e247572d5c262850277707923ee6
maciej <mkrywult88@gmail.com>
2022-09-04 15:57:10 +0200

    Q71

--- exercises/Day_18.py
+++ exercises/Day_18.py
@@ -2,3 +2,8 @@
 import random #only for exercise here
 rand = [i for i in range(0, 11, 2)]
 print(random.choice(rand))
+
+#Q71:
+rand = [i for i in range(0, 151) if i%5 == 0 and i%7 == 0]
+print(random.choice(rand))
+
