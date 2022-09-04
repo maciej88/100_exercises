a02f1c4af6272be940d7970e82851b0e83ba8846
maciej <mkrywult88@gmail.com>
2022-09-04 16:08:25 +0200

    Q74

--- exercises/Day_18.py
+++ exercises/Day_18.py
@@ -14,3 +14,8 @@ print(rand)
 #Q73:
 rand = random.sample(range(100, 201, 2),5 )
 print(rand)
+
+#Q74:
+lst = [i for i in range(0, 1001) if i%5 == 0 and i%7 == 0]
+rand = random.sample(lst, 5)
+print(rand)
