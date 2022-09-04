9638a6744d099560cb9ea0a3766ed9831a211c39
maciej <mkrywult88@gmail.com>
2022-09-04 16:06:48 +0200

    Q73

--- exercises/Day_18.py
+++ exercises/Day_18.py
@@ -8,5 +8,9 @@ rand = [i for i in range(0, 151) if i%5 == 0 and i%7 == 0]
 print(random.choice(rand))
 
 #Q72:
-rand = random.sample(range(100, 200),5 )
+rand = random.sample(range(100, 201),5 )
+print(rand)
+
+#Q73:
+rand = random.sample(range(100, 201, 2),5 )
 print(rand)
