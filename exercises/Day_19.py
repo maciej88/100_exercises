119ea7c90bfeb5c3df15482b9ebe122d1e0f3667
maciej <mkrywult88@gmail.com>
2022-09-05 20:29:13 +0200

    Q76

--- exercises/Day_19.py
+++ exercises/Day_19.py
@@ -1,4 +1,13 @@
 import random
+import zlib
 
 #Q75:
 print(random.randrange(7, 16))
+
+#Q76:
+s = 'hello world!hello world!hello world!hello world!'
+# In Python 3 zlib.compress() accepts only DataType <bytes>
+y = bytes(s, 'utf-8')
+x = zlib.compress(y)
+print(x)
+print(zlib.decompress(x))
