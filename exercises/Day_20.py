113260c87cf3204f6984dbd97d90651e41fad611
maciej <mkrywult88@gmail.com>
2022-09-06 19:07:05 +0200

    Q82

--- exercises/Day_20.py
+++ exercises/Day_20.py
@@ -9,3 +9,8 @@ print(list(filter(is_even, li)))
 lst = [12,24,35,70,88,120,155]
 lst = [i for i in lst if i%5!=0 and i%7!=0]
 print(lst)
+
+#Q82:
+li = [12,24,35,70,88,120,155]
+li = [li[i] for i in range(len(li)) if i%2 != 0 and i <= 6]
+print(li)
