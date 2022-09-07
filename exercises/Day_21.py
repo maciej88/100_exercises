5eb2fea6a46a56e7c164d61c85f1ab085dd73ca7
maciej <mkrywult88@gmail.com>
2022-09-07 18:32:32 +0200

    Q85

--- exercises/Day_21.py
+++ exercises/Day_21.py
@@ -1,4 +1,8 @@
 #Q85:
 li = [12,24,35,70,88,120,155]
+lst = [li[i] for i in range(len(li)) if i not in (0, 4, 5)]
+print(lst)
 new = [x for (i, x) in enumerate(li) if i not in (0, 4, 5)]
 print(new)
+
+#Q86:
