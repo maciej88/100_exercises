48135fe2f982a0d2083a689b0ed061822d0701e4
maciej <mkrywult88@gmail.com>
2022-09-09 18:59:11 +0200

    Q96

--- exercises/Day_23.py
+++ exercises/Day_23.py
@@ -1,6 +1,15 @@
+import textwrap
+
 #Q95:
 x = int(input("Score: "))
 i = map(int, input("scores: ").split())
 i = list(set(i))
 i.sort()
 print(i[-2])
+
+#Q96:
+s = input("String to wrap: ")
+w = int(input("number of letters in wrap: "))
+dspl = textwrap.wrap(s, w)
+for i in dspl:
+    print(i)
