8e5cdd3d61979525044e5afcb5b1ccd21e5dbfdc
maciej <mkrywult88@gmail.com>
2022-09-09 19:34:41 +0200

    Q97

--- exercises/Day_23.py
+++ exercises/Day_23.py
@@ -1,4 +1,5 @@
 import textwrap
+import string
 
 #Q95:
 x = int(input("Score: "))
@@ -13,3 +14,22 @@ w = int(input("number of letters in wrap: "))
 dspl = textwrap.wrap(s, w)
 for i in dspl:
     print(i)
+
+#Q97:
+def rangoli():
+    n = int(input("Size: "))
+    alpha = string.ascii_lowercase
+    w = 4*n - 3
+    r = []
+    for i in range(n):
+       part = '-'.join(alpha[n-i - 1:n])
+       mid = part[-1:0:-1] + part
+       result = mid.center(w, '-')
+       r.append(result)
+    if len(r) > 1:
+        for i in r[n - 2::-1]:
+            r.append(i)
+    r = '\n'.join(r)
+    print(r)
+
+rangoli()
