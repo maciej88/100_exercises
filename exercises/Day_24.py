e860ea0f3502a93633fd6f0188e2a2d1bf7f8bd4
maciej <mkrywult88@gmail.com>
2022-09-10 15:28:44 +0200

    Q101

--- exercises/Day_24.py
+++ exercises/Day_24.py
@@ -14,3 +14,12 @@ print(len(word_list))
 for word in word_list:
     print(word_dict[word], end=' ')
 
+#Q101:
+letters = input()
+d = {}
+for i in letters:
+    d[i] = d.get(i, 0) + 1
+
+d = sorted(d.items(), key=lambda x: (-x[1], x[0]))
+for i in d:
+    print(i[0], i[1])
